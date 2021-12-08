68ff505d13ce1266836d3b8e2a272284dc0682ab
Siddhartha Verma <57825216+SidVer312@users.noreply.github.com>
2021-12-08 18:57:40 +0530

    Add files via upload

---
+++ IP-defang.py
@@ -0,0 +1,8 @@
+def ip_address(address):
+    new_address = ""
+    split_address = address.split(".")
+    separator = "[.]"
+    new_address = separator.join(split_address)
+    return new_address
+ipaddress = ip_address(input("IP adress "))
+print(ipaddress)
